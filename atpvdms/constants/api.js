f42d30791558b372c2ef943d291d145c7d6aab12
Overite <120859629+Overite@users.noreply.github.com>
2024-02-27 23:48:51 +0100

    Update api.js

--- atpvdms/constants/api.js
+++ atpvdms/constants/api.js
@@ -1,3 +1,4 @@
-const SERVER_URL = 'http://localhost:3002/api';
+// const SERVER_URL = 'http://localhost:3002/api';
+const SERVER_URL = 'https://pipeline-vandalization-system-using.onrender.com/api';
 
 export { SERVER_URL }
